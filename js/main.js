7da41a0fe0eb446808b7b1ee54d0c9f383ed701c
AlexeyNZ <alex0220853890@gmail.com>
2016-09-02 15:05:13 +1200

    resize banners

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
   $(document).ready(function(){
-        $("section").each(function(index){
-        $(this).on("click", function(){
-        $('html,body').animate({
+        $("section").each(function(){
+            $(this).on("click", function(){
+                $('html,body').animate({
                 scrollTop: $(this).next().offset().top},
                 'slow');
-        });
+            });
         });
         $(".btnSquare").click(function(event){
         event.stopPropagation();
